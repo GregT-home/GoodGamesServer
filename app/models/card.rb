059164f5887bcb231a4f8e2fcaa9caa00a8fd661
Greg Tarsa <gltarsa@gmail.com>
2014-03-19 01:05:07 -0400

    more changes to support different card styles

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,11 +1,9 @@
 class Card
   attr_reader :rank, :suit
 
-  # define constants so we can use the same literal string everywhere
-#  RANKS = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-#  SUITS = %w(C D H S)
-  RANKS = %(carrot sun star watermelon banana pin umbrella cloud button heart flower screw moon).split(" ")
-  SUITS = %w(A B C D)
+  # define constants so we can use the same literal strings in deck
+  RANKS = %w(2 3 4 5 6 7 8 9 10 j q k a)
+  SUITS = %w(c d h s)
 
   def initialize(myrank, mysuit)
     @rank = myrank
@@ -17,16 +15,9 @@ end
   end
 
   def to_s
-    "#{@rank}-#{@suit}"
+    "#{@rank.upcase}-#{@suit.upcase}"
   end
 
-  # test this exhaustively
-  # as_1 = Card("A", "S"); as_2 = Card("A", "S")
-  # hash { as_1 => 'Wild')
-  # expect(as1).to eql(as2)
-  # expect(as1).to equal (as2)
-  # expect(as1).to eql? (as2)
-  # check Ken's slides for actual test set
   def == card
     rank == card.rank && suit == card.suit
   end
