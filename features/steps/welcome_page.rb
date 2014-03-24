193023955d7383605db75328684e084574407019
Greg Tarsa <gltarsa@gmail.com>
2014-03-24 14:04:46 -0400

    Fix broken welcome page and associated tests

--- features/steps/welcome_page.rb
+++ features/steps/welcome_page.rb
@@ -8,7 +8,35 @@ class Spinach::Features::WelcomePage < Spinach::FeatureSteps
     expect(page).to have_title("Good Games")
   end
 
-  step 'be able to sign-in' do
-    expect(find ".main-choices").to have_link("Sign In", href: user_session_path)
+  step 'they should see a navigation bar' do
+    expect(page).to have_css('nav')
+  end
+
+  step 'it contains a Home link' do
+    expect(find ".navbar-nav").to have_link("Home", href: root_path)
+  end
+
+  step 'a Sign in link' do
+    expect(find ".navbar-nav").to have_link("Sign in", href: new_user_session_path)
+  end
+
+  step 'a Help Link' do
+    expect(find ".navbar-nav").to have_link("Help", href: "#tbi")
+  end
+
+  step 'does not contain a Play Link' do
+    expect(find ".navbar-nav").not_to have_link("Play")
+  end
+
+ step 'they should see a welcome message' do
+    expect(page).to have_selector "h1#main-choices-title", text: "Welcome to Good Games"
+  end
+
+  step 'a choice to Sign up' do
+    expect(find ".main-choices").to have_link("Sign up", href: new_user_registration_path)
+  end
+
+  step 'a choice to Sign in' do
+    expect(find ".main-choices").to have_link("Sign in", href: new_user_session_path)
   end
 end
